302fb5ae5707efad9d61701892cfc3d5fd158819
Hanu Kancharla <kancharla.hanu@gmail.com>
2022-04-21 23:18:27 -0700

    Added Movement Script

--- Story Prompt/Assets/Scripts/PlayerController.cs
+++ Story Prompt/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    float playerSpeed = 5f;
+
+    Vector3 moveVector;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
     }
+
+    void PlayerInput()
+    {
+        //Movement X Axis
+        if (Input.GetKey(KeyCode.A)) moveVector.x = -1;
+        else if (Input.GetKey(KeyCode.D)) moveVector.x = 1;
+        else moveVector.x = 0;
+
+        //Movement Y Axis
+        if (Input.GetKey(KeyCode.W)) moveVector.y = 1;
+        else if (Input.GetKey(KeyCode.S)) moveVector.y = -1;
+        else moveVector.y = 0;
+    }
+
+    void PlayerMovement()
+    {
+        PlayerInput();
+        transform.position += moveVector.normalized * playerSpeed * Time.deltaTime;
+    }
+
 }
