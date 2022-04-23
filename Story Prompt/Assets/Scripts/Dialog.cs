6441d0934458ec8ad72220f14735d9e90af187d7
LuLegendary <asd9872823@gmail.com>
2022-04-23 15:02:42 -0700

    Dialogue & path
    still working on it

--- Story Prompt/Assets/Scripts/Dialog.cs
+++ Story Prompt/Assets/Scripts/Dialog.cs
@@ -7,6 +7,7 @@ public class Dialog : MonoBehaviour
 {
 
     public TextMeshProUGUI dialogText;
+    public GameObject nextLine;
     public float typingSpeed = 0.2f;
     public List<string> dialogs = new List<string>();
     public GameObject continueButton;
@@ -16,8 +17,11 @@ public class Dialog : MonoBehaviour
     public float enablePlayerControllerTimer = 0.5f;
     public float enableColliderTimer = 5f;
 
+    bool canDialogue = false;
+    bool dialoguing = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
-    {
+    {/*
         if(collision.gameObject.tag == "Player")
         {
             StartCoroutine(PrintDialog());
@@ -26,15 +30,31 @@ public class Dialog : MonoBehaviour
             playerController.moveVector = Vector3.zero;
             playerController.playerAnim.SetBool("Dialog", true);
             playerController.enabled = false;
-        }
+        }*/
+        playerController = collision.gameObject.GetComponent<PlayerController>();
+        canDialogue = true;
     }
 
     private void Update()
     {
-        if(dialogText.text == dialogs[index])
+        /*if(dialogText.text == dialogs[index])
         {
             continueButton.SetActive(true);
-        } 
+        }*/
+
+        if (canDialogue && !dialoguing && Input.GetKeyDown(KeyCode.Space)) {
+            nextLine.SetActive(true);
+            StartCoroutine(PrintDialog());
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            //playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.moveVector = Vector3.zero;
+            playerController.playerAnim.SetBool("Dialog", true);
+            playerController.enabled = false;
+        }
+
+        if (dialoguing && Input.GetKeyDown(KeyCode.Space)) {
+            NextSentence();
+        }
     }
 
     IEnumerator PrintDialog()
@@ -45,6 +65,7 @@ public class Dialog : MonoBehaviour
 
             yield return new WaitForSeconds(typingSpeed);
         }
+        dialoguing = true;
     }
 
     public void NextSentence()
@@ -61,6 +82,9 @@ public class Dialog : MonoBehaviour
             dialogText.text = "";
             continueButton.SetActive(false);
             index = 0;
+            dialoguing = false;
+            canDialogue = false;
+            nextLine.SetActive(false);
             StartCoroutine(EnablePlayerController());
             StartCoroutine(EnableCollider());
         }
