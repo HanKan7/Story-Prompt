d3f2c34fbd84bcbc451b3cc88d9defdbd069b0f1
LuLegendary <asd9872823@gmail.com>
2022-04-23 17:03:45 -0700

    Enemy, GameManager, dialogue

---
+++ Story Prompt/Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public EnemyPaths[] paths;
+    public GameObject enemyPrefab;
+
+    public int numberOfEnemies;
+    public float spawnEnemyTime = 2;
+    float spawnTimeCount = 0;
+
+    public bool enemySpawning = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemySpawning) {
+            SpawnEnemy();
+            
+        }
+    }
+
+    void SpawnEnemy() {
+        if (spawnTimeCount < spawnEnemyTime)
+        {
+            spawnTimeCount += Time.deltaTime;
+        }
+        else {
+            for (int i = 0; i < paths.Length; i++) {
+                Vector3 spawnPoint = paths[i].enemyPath.transform.GetChild(0).transform.position;
+                GameObject tempEnemyPrefab = Instantiate(enemyPrefab, spawnPoint, Quaternion.identity);
+                EnemyMovement tempEnemy = tempEnemyPrefab.GetComponent<EnemyMovement>();
+                tempEnemy.path = paths[i].enemyPath;
+                tempEnemy.targetBuilding = paths[i].targetBuilding;
+            }
+            spawnTimeCount = 0;
+        }
+    }
+
+
+}
+
+[System.Serializable]
+public class EnemyPaths {
+    public GameObject enemyPath;
+    public GameObject targetBuilding;
+}
